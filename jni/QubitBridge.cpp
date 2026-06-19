5ce0aab9060f3152579f7aa93da65ba8f5fcffd7
Redhood <128892414+KIKATA12@users.noreply.github.com>
2026-06-19 14:35:08 +0300

    Refactor JNI function names for Qubit class

--- jni/QubitBridge.cpp
+++ jni/QubitBridge.cpp
@@ -2,31 +2,31 @@
 #include "../core/qubit/Qubit.h"
 
 extern "C" JNIEXPORT jlong JNICALL
-Java_core_Qubit_qubitCreate(JNIEnv* env, jobject obj){
+Java_core_qubit_Qubit_qubitCreate(JNIEnv* env, jobject obj){
   Qubit* q = new Qubit();
   return reinterpret_cast<jlong>(q);
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_core_Qubit_qubitInitialize(JNIEnv* env, jobject obj, jlong ptr, jint state){
+Java_core_qubit_Qubit_qubitInitialize(JNIEnv* env, jobject obj, jlong ptr, jint state){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   q->initialize(state);
 }
 
 extern "C" JNIEXPORT jint JNICALL
-Java_core_QubitMeasure(JNIEnv* env, jobject obj, jlong ptr){
+Java_core_qubit_Qubit_qubitMeasure(JNIEnv* env, jobject obj, jlong ptr){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   return q->measure();
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_core_Qubit_qubitReset(JNIEnv* env,jobject obj, jlong ptr){
+Java_core_qubit_Qubit_qubitReset(JNIEnv* env,jobject obj, jlong ptr){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   q->reset();
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_core_Qubit_qubitFree(JNIEnv* env, jobject obj, jlong ptr){
+Java_core_qubit_Qubit_qubitFree(JNIEnv* env, jobject obj, jlong ptr){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   delete q;
 }
