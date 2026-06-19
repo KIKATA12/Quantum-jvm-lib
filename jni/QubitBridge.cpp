bb67191a8bb757306be15bfc20231484f016ed01
Redhood <128892414+KIKATA12@users.noreply.github.com>
2026-06-19 15:20:40 +0300

    Change JNI method signatures to use 'wrappers' package

--- jni/QubitBridge.cpp
+++ jni/QubitBridge.cpp
@@ -2,31 +2,31 @@
 #include "../core/qubit/Qubit.h"
 
 extern "C" JNIEXPORT jlong JNICALL
-Java_Java_0002dapis_Qubit_qubitMeasure(JNIEnv* env, jobject obj){
+Java_wrappers_Qubit_qubitMeasure(JNIEnv* env, jobject obj){
   Qubit* q = new Qubit();
   return reinterpret_cast<jlong>(q);
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_Java_0002dapis_Qubit_qubitInitialize(JNIEnv* env, jobject obj, jlong ptr, jint state){
+Java_wrappers_Qubit_qubitInitialize(JNIEnv* env, jobject obj, jlong ptr, jint state){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   q->initialize(state);
 }
 
 extern "C" JNIEXPORT jint JNICALL
-Java_Java_0002dapis_Qubit_qubitMeasure(JNIEnv* env, jobject obj, jlong ptr){
+Java_wrappers_Qubit_qubitMeasure(JNIEnv* env, jobject obj, jlong ptr){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   return q->measure();
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_Java_0002dapis_Qubit_qubitReset(JNIEnv* env,jobject obj, jlong ptr){
+Java_wrappers_Qubit_qubitReset(JNIEnv* env,jobject obj, jlong ptr){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   q->reset();
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_Java_0002dapis_Qubit_qubitFree(JNIEnv* env, jobject obj, jlong ptr){
+Java_wrappers_Qubit_qubitFree(JNIEnv* env, jobject obj, jlong ptr){
   Qubit* q = reinterpret_cast<Qubit*>(ptr);
   delete q;
 }
