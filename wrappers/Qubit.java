d93ee0c95e70d1848b7e073486346c6ede427f63
Redhood <128892414+KIKATA12@users.noreply.github.com>
2026-06-19 16:06:14 +0300

    Update Qubit.java

--- wrappers/Qubit.java
+++ wrappers/Qubit.java
@@ -1,1 +1,32 @@
+package wrappers;
 
+public class Qubit implements AutoCloseable {
+ static {
+   System.loadLibrary("QubitNative");
+ }
+  private long nativePtr;
+  public Qubit() {
+    nativePtr = qubitCreate();
+  }
+  public void initialize(int state){
+    qubitInitialize(nativePtr, state);
+  }
+  public int measure(){
+    return qubitMeasure(nativePtr);
+  }
+  public void reset(){
+    qubitReset(nativePtr);
+  }
+  @override
+  public void close(){
+    if (nativePtr != 0){
+      qubitFree(nativePtr);
+      nativePtr = 0;
+    }
+  }
+  private native long qubitCreate();
+  private native void qubitInitialize(long ptr, int state);
+  private native int qubitMeasure(long ptr);
+  private native void qubitReset(long ptr);
+  private native void qubitFree(long ptr);
+}
