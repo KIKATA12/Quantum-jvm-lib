d655a8fb7f486b68f86752aee394026c7444d139
Redhood <128892414+KIKATA12@users.noreply.github.com>
2026-06-20 12:36:00 +0300

    Rename JNI methods for Qubit operations

--- jni/QubitBridge.cpp
+++ jni/QubitBridge.cpp
@@ -4,22 +4,22 @@
 static QubitRegistry registry;
 extern "C" {
   JNIEXPORT jlong JNICALL
-  Java_wrappers_Qubit_qubitCreate(JNIEnv* env, jobject obj, jint state){
+  Java_wrappers_Qubit_createQubit(JNIEnv* env, jobject obj, jint state){
     return registry.createQubit(state);
   }
   
   JNIEXPORT jint JNICALL
-  Java_wrappers_Qubit_qubitMeasure(JNIEnv* env, jobject obj, jint id){
+  Java_wrappers_Qubit_measureQubit(JNIEnv* env, jobject obj, jint id){
     return registry.measureQubit(id);
   }
   
   JNIEXPORT void JNICALL
-  Java_wrappers_Qubit_qubitReset(JNIEnv* env,jobject obj, jint id){
+  Java_wrappers_Qubit_resetQubit(JNIEnv* env,jobject obj, jint id){
     registry.resetQubit(id);
   }
   
   JNIEXPORT void JNICALL
-  Java_wrappers_Qubit_qubitRelease(JNIEnv* env, jobject obj, jint id){
+  Java_wrappers_Qubit_releaseQubit(JNIEnv* env, jobject obj, jint id){
     registry.releaseQubit(id);
   }
 }
