3f69bc7b5609a68b78d07f0cca764b8f0c4cc693
Redhood <128892414+KIKATA12@users.noreply.github.com>
2026-06-20 11:15:44 +0300

    Implement QubitRegistry class for Qubit management
    Added QubitRegistry class to manage Qubit instances.

--- core/registers/QubitRegistry.h
+++ core/registers/QubitRegistry.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "../qubit/Qubit.h"
+#include <map>
+#include <memory>
+#include <iostream>
+
+class QubitRegistry {
+private:
+  std::map<int, std::unique_ptr<Qubit>> qubits;
+  int nextId = 0;
+public:
+  int createQubit(int state = 0);
+  Qubit* getQubit(int id);
+  int measureQubit(int id);
+  void resetQubit(int id);
+  void releaseQubit(int id);
+  void printQubit(int id);
+};
